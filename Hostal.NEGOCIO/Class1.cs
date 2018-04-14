d2c93ceaee4ac6292b05d5f770f911e8eecbb74c
Vogoxi <German.Aguilera@users.noreply.github.com>
2018-04-14 17:25:41 -0300

    Agregar archivos de proyecto.

---
+++ Hostal.NEGOCIO/Class1.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Hostal.NEGOCIO
+{
+    public class Usuario
+    {
+        private int _id;
+        private string _usuario, _contrasena;
+        private char _tipoUsuario;
+
+        public Usuario()
+        {
+            Init();
+        }
+
+        private void Init()
+        {
+            _id = 0;
+            _usuario = string.Empty;
+            _contrasena = string.Empty;
+            _tipoUsuario = ' ';
+        }
+    }
+}
