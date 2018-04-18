e626895e860f9f6ff6eeb521adce852448320b66
Vogoxi <German.Aguilera@users.noreply.github.com>
2018-04-17 21:10:53 -0300

    Usuario Collection
    Se genera Usuario Collection

---
+++ Hostal.NEGOCIO/UsuarioCollection.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Hostal.NEGOCIO
+{
+    public class UsuarioCollection
+    {
+        public UsuarioCollection()
+        {
+
+        }
+
+
+        private List<NEGOCIO.Usuario> GenerarListado
+            (List<DALC.USUARIO> UsuarioDALC)
+        {
+            List<NEGOCIO.Usuario> Usuarios =
+                new List<NEGOCIO.Usuario>();
+
+            foreach (DALC.USUARIO item in UsuarioDALC)
+            {
+                NEGOCIO.Usuario UserTemp = new NEGOCIO.Usuario();
+                UserTemp.Id = (int)item.ID;
+                UserTemp.Contrasena = item.CONTRASENA;
+                UserTemp.User = item.USUARIO1;
+                UserTemp.TipoUsuario = item.TIPO_USUARIO;
+
+
+                Usuarios.Add(UserTemp);
+
+            }
+
+            return Usuarios;
+        }
+
+        public List<NEGOCIO.Usuario> ReadAll()
+        {
+            var reconversions = CommonBC.Modelo.USUARIO;
+            return GenerarListado(reconversions.ToList());
+        }
+    }
+}
