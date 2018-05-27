8abae1a158f17bacf429dcaea87aefa12f3cdc74
Poyokos <Poyokos@users.noreply.github.com>
2018-05-27 17:03:26 -0400

    Bootstrap and menu desing

--- Hostal.WEB/SignUp.aspx.designer.cs
+++ Hostal.WEB/SignUp.aspx.designer.cs
@@ -31,6 +31,15 @@ namespace Hostal.WEB {
         protected global::System.Web.UI.WebControls.TextBox txtPassword;
         
         /// <summary>
+        /// txtRepPassword control.
+        /// </summary>
+        /// <remarks>
+        /// Auto-generated field.
+        /// To modify move field declaration from designer file to code-behind file.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.TextBox txtRepPassword;
+        
+        /// <summary>
         /// txtRut control.
         /// </summary>
         /// <remarks>
