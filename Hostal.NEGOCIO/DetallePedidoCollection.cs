3685d31356b7903f5316f1c5e4a7f5f3199e495c
Poyokos <Poyokos@users.noreply.github.com>
2018-06-19 20:40:02 -0400

    Clase pedido y detalle_pedido

---
+++ Hostal.NEGOCIO/DetallePedidoCollection.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Hostal.NEGOCIO
+{
+    public class DetallePedidoCollection
+    {
+        public DetallePedidoCollection()
+        {
+
+        }
+
+        private List<NEGOCIO.DetallePedido> GenerarListado
+            (List<DALC.DETALLE_PEDIDO> DetallePedidoDALC)
+        {
+            List<NEGOCIO.DetallePedido> DetallePedido =
+                new List<NEGOCIO.DetallePedido>();
+
+            foreach (DALC.DETALLE_PEDIDO item in DetallePedidoDALC)
+            {
+                NEGOCIO.DetallePedido PedTemp = new NEGOCIO.DetallePedido();
+                PedTemp.Id = (int)item.ID;
+                PedTemp.Idpedido = (int)item.ID_PEDIDO;
+                PedTemp.Cantidad = (int)item.CANTIDAD;
+                PedTemp.Producto = item.PRODUCTO;
+
+                DetallePedido.Add(PedTemp);
+
+            }
+
+            return DetallePedido;
+        }
+
+        public List<NEGOCIO.DetallePedido> ReadAll()
+        {
+            var reconversions = CommonBC.Modelo.DETALLE_PEDIDO;
+            return GenerarListado(reconversions.ToList());
+        }
+    }
+}
---
+++ Hostal.NEGOCIO/Pedido.cs
@@ -0,0 +1,213 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Hostal.NEGOCIO
+{
+    public class Pedido
+    {
+        private string _idProveedor;
+        private int _nPedido, _idEmpleado;
+        private DateTime _fechaEmision, _fechaEntrega;
+
+        public Pedido()
+        {
+            NPedido = 0;
+            IdEmpleado = 0;
+            IdProveedor = "";
+            FechaEmision = DateTime.Today;
+            FechaEntrega = DateTime.Today;
+        }
+        public int NPedido
+        {
+            get
+            {
+                return _nPedido;
+            }
+
+            set
+            {
+                _nPedido = value;
+            }
+        }
+
+        public int IdEmpleado
+        {
+            get
+            {
+                return _idEmpleado;
+            }
+
+            set
+            {
+                _idEmpleado = value;
+            }
+        }
+
+        public string IdProveedor
+        {
+            get
+            {
+                return _idProveedor;
+            }
+
+            set
+            {
+                _idProveedor = value;
+            }
+        }
+
+        public DateTime FechaEmision
+        {
+            get
+            {
+                return _fechaEmision;
+            }
+
+            set
+            {
+                _fechaEmision = value;
+            }
+        }
+
+        public DateTime FechaEntrega
+        {
+            get
+            {
+                return _fechaEntrega;
+            }
+
+            set
+            {
+                _fechaEntrega = value;
+            }
+        }
+
+        public bool AgregarPedido()
+        {
+            DALC.PEDIDO pedido = new DALC.PEDIDO();
+
+            try
+            {
+                // ID por trigger
+                pedido.FECHA_EMISION = this.FechaEmision;
+                pedido.FECHA_ENTREGA = this.FechaEntrega;
+                pedido.ID_EMPLEADO = this.IdEmpleado;
+                pedido.ID_PROVEEDOR = this.IdProveedor;
+
+                CommonBC.Modelo.PEDIDO.Add(pedido);
+                CommonBC.Modelo.SaveChanges();
+                return true;
+
+            }
+            catch (Exception ex)
+            {
+                CommonBC.Modelo.PEDIDO.Remove(pedido);
+                Logger.Log("Pedido.cs");
+                Logger.Log(ex.Message);
+                if (ex.InnerException != null)
+                {
+                    Logger.Log(ex.InnerException.ToString());
+                }
+                return false;
+            }
+        }
+
+
+        public Pedido GetPedido()
+        {
+            DALC.PEDIDO pedido = new DALC.PEDIDO();
+
+            try
+            {
+                pedido = CommonBC.Modelo.PEDIDO.FirstOrDefault(r => this.NPedido == r.N_PEDIDO);
+
+                if (pedido != null)
+                {
+                    Pedido pla = new Pedido();
+
+                    pla.NPedido = (int)pedido.N_PEDIDO;
+                    pla.IdEmpleado = (int)pedido.ID_EMPLEADO;
+                    pla.IdProveedor = pedido.ID_PROVEEDOR;
+                    pla.FechaEmision = (DateTime)pedido.FECHA_EMISION;
+                    pla.FechaEntrega = (DateTime)pedido.FECHA_ENTREGA;
+
+                    return pla;
+                }
+                else
+                {
+                    return null;
+                }
+
+            }
+            catch (Exception ex)
+            {
+                Logger.Log("Pedido.cs");
+                Logger.Log(ex.Message);
+                if (ex.InnerException != null)
+                {
+                    Logger.Log(ex.InnerException.ToString());
+                }
+                return null;
+            }
+        }
+
+        public bool ActualizarPedido()
+        {
+            DALC.PEDIDO pedido = new DALC.PEDIDO();
+
+            try
+            {
+                pedido = CommonBC.Modelo.PEDIDO.FirstOrDefault(r => this.NPedido == r.N_PEDIDO);
+
+
+                pedido.ID_EMPLEADO = this.IdEmpleado;
+                pedido.ID_PROVEEDOR = this.IdProveedor;
+                pedido.FECHA_EMISION = this.FechaEmision;
+                pedido.FECHA_ENTREGA = this.FechaEntrega;
+
+                CommonBC.Modelo.SaveChanges();
+                return true;
+            }
+            catch (Exception ex)
+            {
+                Logger.Log("Pedido.cs");
+                Logger.Log(ex.Message);
+                if (ex.InnerException != null)
+                {
+                    Logger.Log(ex.InnerException.ToString());
+                }
+                return false;
+            }
+        }
+
+        public bool BorrarPedido()
+        {
+            DALC.PEDIDO pedido = new DALC.PEDIDO();
+
+            try
+            {
+                pedido = CommonBC.Modelo.PEDIDO.FirstOrDefault(r => this.NPedido == r.N_PEDIDO);
+
+                CommonBC.Modelo.PEDIDO.Remove(pedido);
+                CommonBC.Modelo.SaveChanges();
+                return true;
+            }
+            catch (Exception ex)
+            {
+                Logger.Log("Pedido.cs");
+                Logger.Log(ex.Message);
+                if (ex.InnerException != null)
+                {
+                    Logger.Log(ex.InnerException.ToString());
+                }
+                return false;
+            }
+        }
+
+
+
+    }
+}
