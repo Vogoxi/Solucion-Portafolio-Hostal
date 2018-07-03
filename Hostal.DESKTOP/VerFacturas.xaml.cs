7daac41646ed65e6a4886caaf2e8694beded92e5
Poyokos <Poyokos@users.noreply.github.com>
2018-07-03 15:48:18 -0400

    factura y detalle de factura

--- Hostal.DESKTOP/VerFacturas.xaml.cs
+++ Hostal.DESKTOP/VerFacturas.xaml.cs
@@ -35,11 +35,29 @@ namespace Hostal.DESKTOP
             List<NEGOCIO.Factura> facturas = new List<NEGOCIO.Factura>();
             NEGOCIO.FacturaCollection colection = new NEGOCIO.FacturaCollection();
             facturas = colection.ReadAll();
+            facturas.OrderBy(f => f.Id);
             dta_Facturas.ItemsSource = facturas;
+
+            dta_Detalle.CanUserAddRows = false;
+            dta_Facturas.CanUserAddRows = false;
 
             //dta_Facturas.Columns[0].Header = "N° Factura";
             //dta_Facturas.Columns[1].Header = "Empresa";
             //dta_Facturas.Columns[1].Header = "Empresa";
+        }
+
+        private void dta_Facturas_SelectionChanged(object sender, SelectionChangedEventArgs e)
+        {
+            NEGOCIO.Factura factura = dta_Facturas.SelectedItem as NEGOCIO.Factura;
+
+            List<NEGOCIO.DetalleFactura> detalles = new List<NEGOCIO.DetalleFactura>();
+            List<NEGOCIO.DetalleFactura> detallesGrid = new List<NEGOCIO.DetalleFactura>();
+            NEGOCIO.DetalleFacturaCollection colection = new NEGOCIO.DetalleFacturaCollection();
+
+            detalles = colection.ReadById(factura.Id);
+            detalles.OrderBy(d => d.Id);
+
+            dta_Detalle.ItemsSource = detalles;
         }
     }
 }
